a1040eef00e33be325703a0d4bddbd2baf9f07f9
Davidjohndeleon <131321437+Davidjohndeleon@users.noreply.github.com>
2023-11-14 14:00:07 +0800

    Update orderdetailController.php

--- app/Http/Controllers/orderdetailController.php
+++ app/Http/Controllers/orderdetailController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\orderdetail;
 use Illuminate\Http\Request;
-use App\Models\orderdetail;  
+use App\Models\orderdetails;  
 
 
 class orderdetailController extends Controller
 {
     public function index(){
-        return view('orderdetail')->with(["orderdetails" => OrderDetail::first()]);
+        return view('orderdetail')->with(["orderdetails" => orderdetails::first()]);
     }
 }
